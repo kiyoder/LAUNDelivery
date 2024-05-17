c222ba53bba6180eeb1e510119cf1bacafcbc5b4
moriel <morielbien101@gmail.com>
2024-05-17 09:21:04 +0800

    Login.java fixed

--- app/src/main/java/com/example/laundelivery/Login.java
+++ app/src/main/java/com/example/laundelivery/Login.java
@@ -7,6 +7,6 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Login extends AppCompatActivity {
     protected void OnCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_logi);
     }
 }
