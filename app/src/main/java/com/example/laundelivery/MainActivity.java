c6df7c596182c60fcea2f28903b39539feb3806d
ellydhore <ellydhoregabrylle.belleza@gmail.com>
2024-05-17 19:21:20 +0800

    Ladies and Gentlemen, registration and login functionalities have now been implemented. But at what cost?

--- app/src/main/java/com/example/laundelivery/MainActivity.java
+++ app/src/main/java/com/example/laundelivery/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.laundelivery;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    Button buttonLogout;
+    TextView textViewEmail;
+    FirebaseAuth mAuth;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        buttonLogout = findViewById(R.id.buttonLogout);
+        textViewEmail = findViewById(R.id.textViewEmail);
+        user = mAuth.getCurrentUser();
+
+        if(user == null) {
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+            finish();
+        } else {
+            textViewEmail.setText(user.getEmail());
+        }
+
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
