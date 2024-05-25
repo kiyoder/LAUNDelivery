d2e73115e396a1ea8582954479574e8160254282
Kiyokun <yoshinori.kyonojr@cit.edu>
2024-05-25 21:15:16 +0800

    login redesign and homepage

--- app/src/main/java/com/example/laundelivery/Home.java
+++ app/src/main/java/com/example/laundelivery/Home.java
@@ -2,13 +2,38 @@ package com.example.laundelivery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button btnLoginHome = findViewById(R.id.btnLoginHome);
+        btnLoginHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btnSignupHome = findViewById(R.id.btnSignupHome);
+        btnSignupHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, Registration.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
